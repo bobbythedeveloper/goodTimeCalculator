a166b803fd72b739432daff3be113f8074352e84
pubby-git <prabhanjan.nagendra@gmail.com>
2016-12-26 17:10:52 -0500

    added few more changes

--- goodTimes/startserver.go
+++ goodTimes/startserver.go
@@ -2,13 +2,28 @@ package hello
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
 func init() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/getKaalam", getKaalam)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello, world!")
+}
+
+func getKaalam(w http.ResponseWriter, r *http.Request) {
+	latitude := float64(5000)
+	longitude := float64(6000)
+
+	sunrise, sunset := getSunriseAndSunset(latitude, longitude)
+	log.Println(sunrise)
+	log.Println(sunset)
+}
+func getSunriseAndSunset(lat float64, longt float64) (string, string) {
+
+	// call backend and pass the latigude and logitude
+	return "", ""
 }
