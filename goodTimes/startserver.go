f78cac6865ff19121125e567c7bc34a90d422ea0
pubby-git <prabhanjan.nagendra@gmail.com>
2016-12-26 18:12:49 -0500

    few more fixes

--- goodTimes/startserver.go
+++ goodTimes/startserver.go
@@ -1,9 +1,14 @@
 package hello
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/urlfetch"
 )
 
 func init() {
@@ -11,18 +16,23 @@ func init() {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
+
 	fmt.Fprint(w, "Hello, world!")
 }
 
 func getKaalam(w http.ResponseWriter, r *http.Request) {
+	ctx := appengine.NewContext(r)
+	client := urlfetch.Client(ctx)
+
 	latitude := float64(36.7201600)
 	longitude := float64(-4.4203400)
 
-	sunrise, sunset := getSunriseAndSunset(latitude, longitude)
+	sunrise, sunset := getSunriseAndSunset(latitude, longitude, client)
 	log.Println(sunrise)
 	log.Println(sunset)
 }
-func getSunriseAndSunset(lat float64, longt float64) (string, string) {
+
+func getSunriseAndSunset(lat float64, longt float64, client *http.Client) (string, string) {
 	latstr := fmt.Sprintf("%f", lat)
 	longstr := fmt.Sprintf("%f", longt)
 
@@ -34,11 +44,40 @@ func getSunriseAndSunset(lat float64, longt float64) (string, string) {
 	q := req.URL.Query()
 	q.Add("lat", latstr)
 	q.Add("lng", longstr)
-
 	req.URL.RawQuery = q.Encode()
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Println("Issue calling backend webservice")
+		log.Fatal(err)
 
-	fmt.Println(req.URL.String())
-
+	}
+	body1, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Println("Cannot read http body")
+	}
+	var backendResponse *sunRiseSunSet
+	err = json.Unmarshal(body1, &backendResponse)
+	if err != nil {
+		log.Println("errpr")
+		log.Fatal(err)
+	}
+	log.Println(backendResponse.Results.Sunrise)
 	// call backend and pass the latigude and logitude
 	return "", ""
+}
+
+type sunRiseSunSet struct {
+	Results struct {
+		Sunrise                   string `json:"sunrise"`
+		Sunset                    string `json:"sunset"`
+		SolarNoon                 string `json:"solar_noon"`
+		DayLength                 string `json:"day_length"`
+		CivilTwilightBegin        string `json:"civil_twilight_begin"`
+		CivilTwilightEnd          string `json:"civil_twilight_end"`
+		NauticalTwilightBegin     string `json:"nautical_twilight_begin"`
+		NauticalTwilightEnd       string `json:"nautical_twilight_end"`
+		AstronomicalTwilightBegin string `json:"astronomical_twilight_begin"`
+		AstronomicalTwilightEnd   string `json:"astronomical_twilight_end"`
+	} `json:"results"`
+	Status string `json:"status"`
 }
