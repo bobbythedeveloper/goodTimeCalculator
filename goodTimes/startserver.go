cab371108e2bc98c2e62c5d49d63d162d0e65b7c
pubby-git <prabhanjan.nagendra@gmail.com>
2016-12-26 17:28:25 -0500

    adding function to call backend

--- goodTimes/startserver.go
+++ goodTimes/startserver.go
@@ -15,14 +15,29 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func getKaalam(w http.ResponseWriter, r *http.Request) {
-	latitude := float64(5000)
-	longitude := float64(6000)
+	latitude := float64(36.7201600)
+	longitude := float64(-4.4203400)
 
 	sunrise, sunset := getSunriseAndSunset(latitude, longitude)
 	log.Println(sunrise)
 	log.Println(sunset)
 }
 func getSunriseAndSunset(lat float64, longt float64) (string, string) {
+	latstr := fmt.Sprintf("%f", lat)
+	longstr := fmt.Sprintf("%f", longt)
+
+	//http://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400&date=2016-12-26
+	req, err := http.NewRequest("GET", "http://api.sunrise-sunset.org/json", nil)
+	if err != nil {
+		log.Print(err)
+	}
+	q := req.URL.Query()
+	q.Add("lat", latstr)
+	q.Add("lng", longstr)
+
+	req.URL.RawQuery = q.Encode()
+
+	fmt.Println(req.URL.String())
 
 	// call backend and pass the latigude and logitude
 	return "", ""
