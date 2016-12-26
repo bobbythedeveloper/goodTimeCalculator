d926faa6102a596d97173db9a52adfbd8ca37bd2
pubby-git <prabhanjan.nagendra@gmail.com>
2016-12-26 16:41:30 -0500

    first program

--- goodTimes/startserver.go
+++ goodTimes/startserver.go
@@ -1,1 +1,14 @@
-thisis a test
+package hello
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func init() {
+	http.HandleFunc("/", handler)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello, world!")
+}
