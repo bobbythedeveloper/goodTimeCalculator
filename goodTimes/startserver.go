d0d697a591ed5d4582db84bcdd6b79727fa83fcc
pubby-git <prabhanjan.nagendra@gmail.com>
2017-01-01 22:20:52 -0500

    lot of fixes and refactoring of code

--- goodTimes/startserver.go
+++ goodTimes/startserver.go
@@ -22,10 +22,11 @@ func init() {
 
 func handler(w http.ResponseWriter, r *http.Request) {
 
-	fmt.Fprint(w, "Hello, The API usage is /getKaalam?latitude=39.948184&longitude=-78.902575&date=2013-Feb-03")
+	fmt.Fprint(w, "Hello, The API usage is /getKaalam?latitude=39.934002&longitude=-74.89099879999998&date=2017-Jan-02")
 }
 
 func getKaalam(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 	ctx := appengine.NewContext(r)
 	client := urlfetch.Client(ctx)
 	//39.948184, -74.902575
@@ -41,15 +42,16 @@ func getKaalam(w http.ResponseWriter, r *http.Request) {
 		longitude = float64(-74.902575)
 		log.Print(err)
 	}
-	date, err := time.Parse("2006-Jan-02", (r.URL.Query().Get("date")))
+	date, err := time.Parse("2006-Jan-2", (r.URL.Query().Get("date")))
 	if err != nil {
 		log.Println("cannot parse date using default date = today()")
+		date = time.Now()
 		log.Print(err)
 	}
 	//latitude := float64(39.948184)
 	//longitude := float64(-74.902575)
 
-	sunrise, sunset := getSunriseAndSunset(latitude, longitude, client)
+	sunrise, sunset := getSunriseAndSunset(latitude, longitude, date, client)
 	//rahuKaala := getKallas(sunrise, sunset, time.Now())
 	rahuKaala := getKallas(sunrise, sunset, date)
 
@@ -68,18 +70,18 @@ func getKaalam(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, string(jsonresponse))
 
 }
-func getKallas(sunrise string, sunset string, date time.Time) kaalamType {
-	sunriseTime, err := time.Parse("3:04:05 PM", sunrise)
-	if err != nil {
-		log.Println("error parsing time sunriseTime")
-		log.Fatal(err)
-	}
-	sunsetTime, err := time.Parse("3:04:05 PM", sunset)
-	if err != nil {
-		log.Println("error parsing time sunriseTime")
-		log.Fatal(err)
-	}
-	rahuKaalStartAndEndTime := getRahuKaal(sunriseTime, sunsetTime, date)
+func getKallas(sunrise time.Time, sunset time.Time, date time.Time) kaalamType {
+	/*	sunriseTime, err := time.Parse("3:04:05 PM", sunrise)
+		if err != nil {
+			log.Println("error parsing time sunriseTime")
+			log.Fatal(err)
+		}
+		sunsetTime, err := time.Parse("3:04:05 PM", sunset)
+		if err != nil {
+			log.Println("error parsing time sunriseTime")
+			log.Fatal(err)
+		}*/
+	rahuKaalStartAndEndTime := getRahuKaal(sunrise, sunset, date)
 	return rahuKaalStartAndEndTime
 }
 func getRahuKaal(sunrise time.Time, sunset time.Time, date time.Time) kaalamType {
@@ -113,9 +115,10 @@ func getRahuPosition(datetime time.Time) int64 {
 	return 1
 }
 
-func getSunriseAndSunset(lat float64, longt float64, client *http.Client) (string, string) {
+func getSunriseAndSunset(lat float64, longt float64, date time.Time, client *http.Client) (time.Time, time.Time) {
 	latstr := fmt.Sprintf("%f", lat)
 	longstr := fmt.Sprintf("%f", longt)
+	dateString := fmt.Sprintf("%d-%d-%d", date.Year(), date.Month(), date.Day())
 	//http://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400&date=2016-12-26
 	req, err := http.NewRequest("GET", "http://api.sunrise-sunset.org/json", nil)
 	if err != nil {
@@ -124,6 +127,7 @@ func getSunriseAndSunset(lat float64, longt float64, client *http.Client) (strin
 	q := req.URL.Query()
 	q.Add("lat", latstr)
 	q.Add("lng", longstr)
+	q.Add("date", dateString)
 	req.URL.RawQuery = q.Encode()
 	resp, err := client.Do(req)
 	if err != nil {
@@ -141,7 +145,25 @@ func getSunriseAndSunset(lat float64, longt float64, client *http.Client) (strin
 		log.Println("Error understanding backend json")
 		log.Fatal(err)
 	}
-	return backendResponse.Results.Sunrise, backendResponse.Results.Sunset
+	sunriseDateTime, err := time.Parse("3:04:05 PM 2006-1-2", backendResponse.Results.Sunrise+" "+dateString)
+	if err != nil {
+		log.Println("Unable parse sun rise time from backend")
+		log.Println(err)
+	}
+	sunsetDateTime, err := time.Parse("3:04:05 PM 2006-1-2", backendResponse.Results.Sunset+" "+dateString)
+	if err != nil {
+		log.Println("Unable parse sun rise time from backend")
+		log.Println(err)
+	}
+	//loc, err := time.LoadLocation("America/New_York")
+	//if err != nil {
+	//		panic(err)
+	//	}
+	//sunriseDateTime.In(loc)
+	//log.Println(sunriseDateTime.In(loc))
+	//	return backendResponse.Results.Sunrise, backendResponse.Results.Sunset
+	return sunriseDateTime, sunsetDateTime
+
 }
 
 type sunRiseSunSet struct {
